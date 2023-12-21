576c0dea92de92ec0e64e52ccc4d67984e5d5c77
Pao <chaiwatt77@gmail.com>
2023-12-21 14:41:41 +0700

    ex3

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,23 @@
+import {useState} from "react" 
+
 function ProductForm() {
+  const [name,setName] = useState("");
+  const [img,setImg] = useState("");
+  const [price, setPrice] = useState();
+  const [des, setDes] = useState("");
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    alert(`{
+      "name": ${name}
+      "price": ${price}
+      "image": ${img}
+      "description": ${des}
+}`);
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +27,8 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            value={name}
+            onChange={(e) => {setName(e.target.value)}}
           />
         </label>
       </div>
@@ -22,7 +40,8 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            value={img}
+            onChange={(e) => {setImg(e.target.value)}}
           />
         </label>
       </div>
@@ -34,7 +53,8 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            value={price}
+            onChange={(e) => {setPrice(e.target.value)}}
           />
         </label>
       </div>
@@ -46,7 +66,8 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            value={des}
+            onChange={(e) => {setDes(e.target.value)}}
             rows={4}
             cols={30}
           />
@@ -60,3 +81,89 @@ function ProductForm() {
 }
 
 export default ProductForm;
+
+/*
+function ProductForm() {
+  const [name,setName] = useState("");
+  const [img,setImg] = useState("");
+  const [price, setPrice] = useState();
+  const [des, setDes] = useState("");
+  
+  const obj = {};
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    obj.name={name}
+    obj.image={img}
+    obj.price={price}
+    obj.description={des}
+    alert(JSON.stringify(obj));
+  }
+
+  return (
+    <form className="post-form" onSubmit={handleSubmit}>
+      <h1>Create Product Form</h1>
+      <div className="input-container">
+        <label>
+          Name
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Enter name here"
+            value={name}
+            onChange={(e) => {setName(e.target.value)}}
+          />
+        </label>
+      </div>
+      <div className="input-container">
+        <label>
+          Image Url
+          <input
+            id="image"
+            name="image"
+            type="text"
+            placeholder="Enter image url here"
+            value={img}
+            onChange={(e) => {setImg(e.target.value)}}
+          />
+        </label>
+      </div>
+      <div className="input-container">
+        <label>
+          Price
+          <input
+            id="price"
+            name="price"
+            type="number"
+            placeholder="Enter price here"
+            value={price}
+            onChange={(e) => {setPrice(e.target.value)}}
+          />
+        </label>
+      </div>
+      <div className="input-container">
+        <label>
+          Description
+          <textarea
+            id="description"
+            name="description"
+            type="text"
+            placeholder="Enter description here"
+            value={des}
+            onChange={(e) => {setDes(e.target.value)}}
+            rows={4}
+            cols={30}
+          />
+        </label>
+      </div>
+      <div className="form-actions">
+        <button type="submit">Create</button>
+      </div>
+    </form>
+  );
+}
+
+export default ProductForm;
+*/
